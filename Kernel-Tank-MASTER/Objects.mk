0fa666f50c97026f468d771953c6164ff2b9f38f
Anton Gulenko <anton.guleko@student.hpi.uni-potsdam.de>
2012-11-21 14:16:44 +0100

    Added another trick/hack to keep main from rebuilding kernel-project libraries. make doesn't seem to like empty archives, so a phony-object-file is always added to these libraries.

---
+++ Kernel-Tank-MASTER/Objects.mk
@@ -0,0 +1,3 @@
+
+# Make sure at least one object is included in the archive. Otherwise make keeps rebuilding the empty archive.
+objects += Tank-Shared/$(BUILD_DIRNAME)/phony.o
---
+++ Tank-Shared/phony.c
@@ -0,0 +1,3 @@
+
+// This file is empty and serves the purpose to avoid empty archives.
+// The object file of this module is added to kernel-archives (See Object.mk in Kernel-* projects).
