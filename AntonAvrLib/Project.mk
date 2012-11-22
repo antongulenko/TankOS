c1e0ba556a7507bd58b9502321b4d5a8ccc769e9
Anton Gulenko <anton.guleko@student.hpi.uni-potsdam.de>
2012-11-22 20:50:03 +0100

    Moved symbol definition from AntonAvrLib to default tank symbols

--- AntonAvrLib/Project.mk
+++ AntonAvrLib/Project.mk
@@ -2,13 +2,6 @@
 LIBRARY :=
 undefine dependencies
 
-symbols := \
-	TWI_Buffer_Size=255 \
-	millisecond_timer_tick=timer_tick_A
-
-# symbols += FORCE_BUTTON_PIN_CHANGE_INTERRUPTS
-# symbols += KEEP_WDT_ENABLED
-
 BASEDIR := $(get-basedir)
 include Defaults.mk
 include DefaultTankSymbols.mk
