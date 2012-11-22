70fe589c3645013a2540ead2502f0edac7b94f04
Anton Gulenko <anton.guleko@student.hpi.uni-potsdam.de>
2012-11-22 19:19:04 +0100

    Including DefaultTankSymbols also in Main projcets

--- Main-Tank-IO/Project.mk
+++ Main-Tank-IO/Project.mk
@@ -4,4 +4,5 @@ dependencies := AntonAvrLib Tank-Shared Kernel-Tank-IO
 
 BASEDIR := $(get-basedir)
 include Defaults.mk
+include DefaultTankSymbols.mk
 include Main.mk
--- Main-Tank-MASTER/Project.mk
+++ Main-Tank-MASTER/Project.mk
@@ -4,4 +4,5 @@ dependencies := AntonAvrLib Tank-Shared Kernel-Tank-MASTER
 
 BASEDIR := $(get-basedir)
 include Defaults.mk
+include DefaultTankSymbols.mk
 include Main.mk
