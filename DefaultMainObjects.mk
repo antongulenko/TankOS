87b81b3739bca9e54e09eedd29d0155638f3c93f
Anton Gulenko <anton.guleko@student.hpi.uni-potsdam.de>
2012-12-03 10:49:06 +0100

    comment

--- DefaultMainObjects.mk
+++ DefaultMainObjects.mk
@@ -1,8 +1,6 @@
 
 # Include kernel-objects defined by dependency-projects.
-# kernel-objects cannot be linked from within an archive, because then the linker uses them only to resolve symbols!
-# But kernel-objects contain ISRs and .init code sections. The linker must process these things explicitely, which he
+# kernel-objects cannot be linked from within an archive, because then the linker uses them only to resolve symbols.
+# But kernel-objects contain ISRs and .init code sections. The linker must process these things explicitely, which it
 # does only when we pass the object files directly.
 -include $(foreach d, $(dependencies), $d/KernelObjects.mk)
-
-studio_output := $(MAIN_$(project)).main
