db82f3ac823524b0b78fa50490821998b738d31d
Anton Gulenko <anton.guleko@student.hpi.uni-potsdam.de>
2012-12-04 11:42:12 +0100

    ...

--- AntonAvrLib/kernel/devices/analog.h
+++ AntonAvrLib/kernel/devices/analog.h
@@ -39,7 +39,6 @@ BOOL analogRead(PAnalogInput input, AnalogCallbackFunction callback);
 // Return FALSE, if another conversion is currently running.
 BOOL analogReadLoop(PAnalogInput input, uint8_t *result);
 
-
 #define DEFINE_ANALOG_INPUT(analogInputName) extern const PAnalogInput analogInputName;
 
 #define INIT_ANALOG_INPUT(analogInputName, inputPin, pinNumber)	\
