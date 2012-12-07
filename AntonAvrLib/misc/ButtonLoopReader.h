024af57f2e14730cb83990b6a76e714170ead9da
Anton Gulenko <anton.guleko@student.hpi.uni-potsdam.de>
2012-12-07 23:28:13 +0100

    Renamed and refactors button read module.

---
+++ AntonAvrLib/misc/ButtonLoopReader.h
@@ -0,0 +1,25 @@
+#ifndef READ_BUTTONS_LOOP_
+#define READ_BUTTONS_LOOP_
+
+#include "../anton_std.h"
+#include "../kernel/devices/button.h"
+
+DEFINE_HANDLE(ButtonLoopReader);
+
+typedef void (*button_callback)(PButton button);
+
+ButtonLoopReader ButtonLoopReader_Create(
+		uint8_t buttonCount, PButton *buttonArray,
+		button_callback button_pressed,
+		button_callback button_released);
+void ButtonLoopReader_Destroy(ButtonLoopReader reader);
+
+// Enter an eternal loop, reading the status of the given buttons
+// and invoking the weak function button_pressed() whenever a button
+// is pressed. Holding buttons are ignored.
+void ButtonLoopReader_Start(ButtonLoopReader reader);
+
+// Stop this ButtonLoopReader.
+void ButtonLoopReader_Stop(ButtonLoopReader reader);
+
+#endif
