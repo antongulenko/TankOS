176fd562131cbe4fd46a0855f967691f9511b94d
Anton Gulenko <anton.guleko@student.hpi.uni-potsdam.de>
2012-12-07 21:09:59 +0100

    Updated objects used by tests

--- AntonAvrLib-Test/Objects.mk
+++ AntonAvrLib-Test/Objects.mk
@@ -1,6 +1,14 @@
 
 include DefaultObjectVariables.mk
 
-$(eval $(call set_test_objects,port, $(KERNEL)/devices/port.o))
-$(eval $(call set_test_objects,external_interrupts, $(KERNEL)/devices/external_interrupts.o))
-$(eval $(call set_test_objects,led, $(KERNEL)/devices/led.o $(KERNEL)/devices/port.o))
+$(eval $(call set_test_objects,port,\
+	$(KERNEL)/devices/port.o))
+	
+$(eval $(call set_test_objects,external_interrupts,\
+	$(KERNEL)/devices/external_interrupts.o))
+	
+$(eval $(call set_test_objects,led,\
+	$(KERNEL)/devices/led.o $(KERNEL)/devices/port.o))
+	
+$(eval $(call set_test_objects,button,\
+	$(KERNEL)/devices/port.o $(KERNEL)/devices/button.o $(KERNEL)/devices/external_interrupts.o))
