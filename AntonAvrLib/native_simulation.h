9d9eacdd662397d52f2d7b3f81eb722c5bdbfd4e
Anton Gulenko <anton.guleko@student.hpi.uni-potsdam.de>
2012-12-10 23:08:51 +0100

    Added TWI registers to native_simulation

--- AntonAvrLib/native_simulation.h
+++ AntonAvrLib/native_simulation.h
@@ -7,13 +7,63 @@
 
 // This also contains some definitions to make code compile for the native env.
 
+// == Types
 #define _BV(a) (1 << a)
 typedef unsigned int uint32_t;
 typedef unsigned char uint8_t;
+typedef uint8_t REGISTER;
 
-extern uint8_t PCMSK0, PCMSK1, PCMSK2, PCMSK3;
-extern uint8_t PCICR;
-
+// == delay
 void _delay_ms(double ms);
+
+// == External interrupts
+extern REGISTER PCMSK0, PCMSK1, PCMSK2, PCMSK3;
+extern REGISTER PCICR;
+
+// == Atomic
+// TODO -- this should be testable somehow.
+#define ATOMIC_BLOCK(ARG)
+#define ATOMIC_RESTORESTATE
+#define ATOMIC_FORCEON
+
+// == TWI
+#define TWIE 0
+#define TWEN 1
+#define TWSTA 2
+#define TWSTO 3
+#define TWEA 4
+#define TWINT 5
+
+extern REGISTER TWCR, TWDR, TWBR, TWSR, TWAR, TWAMR;
+
+enum {
+	TW_START,
+	TW_REP_START,
+	TW_MR_ARB_LOST,
+	TW_MT_DATA_ACK,
+	TW_MT_SLA_ACK,
+	TW_MT_SLA_NACK,
+	TW_MT_DATA_NACK,
+	TW_MR_SLA_ACK,
+	TW_MR_DATA_ACK,
+	TW_MR_SLA_NACK,
+	TW_MR_DATA_NACK,
+	TW_ST_SLA_ACK,
+	TW_ST_ARB_LOST_SLA_ACK,
+	TW_ST_DATA_ACK,
+	TW_ST_LAST_DATA,
+	TW_ST_DATA_NACK,
+	TW_SR_SLA_ACK,
+	TW_SR_ARB_LOST_SLA_ACK,
+	TW_SR_GCALL_ACK,
+	TW_SR_ARB_LOST_GCALL_ACK,
+	TW_SR_DATA_ACK,
+	TW_SR_GCALL_DATA_ACK,
+	TW_SR_STOP,
+	TW_SR_DATA_NACK,
+	TW_SR_GCALL_DATA_NACK,
+	TW_NO_INFO,
+	TW_BUS_ERROR
+};
 
 #endif
--- AntonAvrLib/native_simulation.kernel.c
+++ AntonAvrLib/native_simulation.kernel.c
@@ -1,8 +1,10 @@
 
 #include "native_simulation.h"
 
-uint8_t PCMSK0, PCMSK1, PCMSK2, PCMSK3;
-uint8_t PCICR;
+REGISTER PCMSK0, PCMSK1, PCMSK2, PCMSK3;
+REGISTER PCICR;
+
+REGISTER TWCR, TWDR, TWBR, TWSR, TWAR, TWAMR;
 
 void _delay_ms(double ms) {
 	// Nothing, just pretend.
