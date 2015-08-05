f149381af1f396a0eca57fb827d86d524f362ce4
Anton Gulenko <anton.gulenko@googlemail.com>
2015-08-06 01:04:49 +0200

    fixed tank code

--- Kernel-Tank-IO/tank_led.kernel.c
+++ Kernel-Tank-IO/tank_led.kernel.c
@@ -15,15 +15,12 @@ LedGroup redLeds, yellowLeds, whiteLeds, greenLeds, leftLeds, middleLeds, rightL
 
 #define ELEMS(arr) (sizeof(arr) / sizeof arr[0])
 
-static Led whiteLedsArray[] = { &whiteLed1, &whiteLed2, &whiteLed3, &whiteLed4, &whiteLed5 };
-static Led redLedsArray[] = { &redLed1, &redLed2, &redLed3, &redLed4, &redLed5 };
-static Led greenLedsArray[] = { &greenLed1, &greenLed2, &greenLed3 };
-static Led yellowLedsArray[] = { &yellowLed1, &yellowLed2 };
-static Led middleLedsArray[] = { &greenLed1, &greenLed2, &greenLed3, &yellowLed1, &yellowLed2 };
-static Led allLedsArray[] = {
-				&whiteLed1, &whiteLed2, &whiteLed3, &whiteLed4, &whiteLed5,
-				&greenLed1, &greenLed2, &greenLed3, &yellowLed1, &yellowLed2,
-				&redLed1, &redLed2, &redLed3, &redLed4, &redLed5 };
+static Led whiteLedsArray[5];
+static Led redLedsArray[5];
+static Led greenLedsArray[3];
+static Led yellowLedsArray[2];
+static Led middleLedsArray[5];
+static Led allLedsArray[];
 
 static void init_tank_leds() {
     whiteLed1 = newLed(pinB0);
@@ -43,6 +40,16 @@ static void init_tank_leds() {
 	greenLed3 = newLed(pinB7);
 	yellowLed1 = newLed(pinD0);
 	yellowLed2 = newLed(pinD1);
+
+    memcpy(whiteLedsArray, (Led[]) { whiteLed1, whiteLed2, whiteLed3, whiteLed4, whiteLed5 }, sizeof(whiteLedsArray));
+    memcpy(redLedsArray, (Led[]) { redLed1, redLed2, redLed3, redLed4, redLed5 }, sizeof(redLedsArray));
+    memcpy(greenLedsArray, (Led[]) { greenLed1, greenLed2, greenLed3 }, sizeof(greenLedsArray));
+    memcpy(yellowLedsArray, (Led[]) { yellowLed1, yellowLed2 }, sizeof(yellowLedsArray));
+    memcpy(middleLedsArray, (Led[]) { greenLed1, greenLed2, greenLed3, yellowLed1, yellowLed2 }, sizeof(middleLedsArray));
+    memcpy(allLedsArray,
+               (Led[]) { whiteLed1, whiteLed2, whiteLed3, whiteLed4, whiteLed5,
+				greenLed1, greenLed2, greenLed3, yellowLed1, yellowLed2,
+				redLed1, redLed2, redLed3, redLed4, redLed5 }, sizeof(allLedsArray));
 
 	redLeds = newLedGroup(redLedsArray, ELEMS(redLedsArray));
 	yellowLeds = newLedGroup(yellowLedsArray, ELEMS(yellowLedsArray));
