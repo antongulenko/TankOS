5b23f15386d463e7d3818aa711870bb8b92a561a
Anton Gulenko <anton.gulenko@googlemail.com>
2015-08-09 19:47:51 +0200

    added twi services to simulator kernel objects.

--- Kernel-Simulator/KernelObjects.mk
+++ Kernel-Simulator/KernelObjects.mk
@@ -8,7 +8,13 @@ ifeq ($(USE_TWI), true)
         $(KERNEL)/twi/driver/slave.kernel.o \
         $(KERNEL)/twi/driver/master.kernel.o \
         $(KERNEL)/twi/rpc/server_handler_functions.kernel.o \
+        $(KERNEL)/twi/services/hardware.kernel.o \
+        $(KERNEL)/twi/services/test.kernel.o \
         $(OWN)/simulator_twi.kernel.o
+
+    ifeq ($(USE_BUFFER_STDOUT), true)
+        objects += $(KERNEL)/twi/services/buffer_stdout.kernel.o
+    endif
 endif
 
 include Tank-Shared/KernelObjects2.mk
