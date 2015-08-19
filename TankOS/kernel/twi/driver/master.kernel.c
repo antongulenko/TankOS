310c290ec339fa7de2eda1fc9a7f4b4f587e99fe
Anton Gulenko <anton.gulenko@googlemail.com>
2015-08-19 14:06:29 +0200

    renamed file

--- TankOS/kernel/twi/driver/master.kernel.c
+++ TankOS/kernel/twi/driver/master.kernel.c
@@ -2,4 +2,4 @@
 #include "master.h"
 #define TWI_HANDLE_FUNCTION twi_handle
 #define TWI_INIT_FUNCTION twi_init
-#include "init.h"
+#include "isr.h"
--- TankOS/kernel/twi/driver/slave.kernel.c
+++ TankOS/kernel/twi/driver/slave.kernel.c
@@ -2,4 +2,4 @@
 #include "slave.h"
 #define TWI_HANDLE_FUNCTION twi_handle_slave
 #define TWI_INIT_FUNCTION twi_init_slave
-#include "init.h"
+#include "isr.h"
