14a95b24c912c6b40146c7c034a67a2d100fbdcf
Anton Gulenko <anton.gulenko@googlemail.com>
2015-08-23 22:44:43 +0200

    Preserving rpc server state on expected twi_error.

--- TankOS/twi/rpc/server.c
+++ TankOS/twi/rpc/server.c
@@ -24,8 +24,14 @@ RpcServerOperation rpc_server_operation = TWI_RPC_waiting;
 RpcServerStatus rpc_server_status = TWI_RPC_no_error;
 
 static void twi_driver_error(RpcServerStatus status) {
-    // This can only happen if something is wrong with the I2C bus.
-    rpc_server_status = twi_error == TWI_No_Error ? status : TWI_RPC_error_driver + twi_error;
+	switch (twi_error) {
+		case TWI_Slave_NotEnoughDataReceived:
+		case TWI_No_Error:
+			rpc_server_status = status;
+			break;
+		default:
+			rpc_server_status = TWI_RPC_error_driver + twi_error;
+	}
 }
 
 static void reset_results() {
