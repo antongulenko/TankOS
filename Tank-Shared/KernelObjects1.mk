fa9de26e2ad1aa7047d3826f9528e50ef2178ffa
Anton Gulenko <anton.gulenko@googlemail.com>
2015-08-24 22:29:56 +0200

    Initializing stdout_buffer earlier during boot. updated project.

--- Tank-Shared/KernelObjects1.mk
+++ Tank-Shared/KernelObjects1.mk
@@ -1,7 +1,13 @@
 
 objects += \
     $(BUILD_TankOS)/kernel/early_init.kernel.o \
-    $(BUILD_TankOS)/kernel/early_init_eeprom.kernel.o \
+    $(BUILD_TankOS)/kernel/early_init_eeprom.kernel.o
+    
+ifeq ($(USE_BUFFER_STDOUT), true)
+    objects += $(BUILD_TankOS)/kernel/buffer_stdout.kernel.o
+endif
+    
+objects += \
     $(BUILD_TankOS)/m1284P/port.kernel.o \
     $(BUILD_Tank-Shared)/timer.kernel.o \
     $(BUILD_TankOS)/mutex/atomic_mutex.kernel.o \
@@ -9,10 +15,6 @@ objects += \
     $(BUILD_TankOS)/devices/analog.kernel.o \
     $(BUILD_TankOS)/m1284P/analog.kernel.o \
     $(BUILD_TankOS)/kernel/klib.kernel.o
-
-ifeq ($(USE_BUFFER_STDOUT), true)
-    objects += $(BUILD_TankOS)/kernel/buffer_stdout.kernel.o
-endif
 
 ifeq ($(USE_TWI), true)
     objects += $(BUILD_Tank-Shared)/twi.kernel.o
