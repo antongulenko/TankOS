3ed5251a34d58d7e8fff25a2b2461e6c90998178
Anton Gulenko <anton.gulenko@googlemail.com>
2015-08-26 22:48:33 +0200

    improved time script

--- TwiTool/scripts/time.sh
+++ TwiTool/scripts/time.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -e
 source common.sh
+test $# == 2 || { echo "2 parameters: <device> and <assumed frequency> "; exit 1; }
 device=$1
-
-ASSUMED_FREQ=8
+ASSUMED_FREQ="$2"
 
 function get_values() {
-    device_time=`twi_call query_milliseconds $1`
+    device_time=`twi_call query_milliseconds $device`
     current_time=`date +%T.%3N`
     device_time=${device_time:9} # Strip "Running: " prefix
     device_millis=`date -d $device_time +%s%3N`
@@ -24,12 +24,7 @@ while true; do
     passed_millis=$((current_millis-start_millis))
     passed_device_millis=$((device_millis-start_device_millis))
     new_diff=$((diff_millis-start_diff))
-    if [ "$passed_device_millis" ]; then
-        freq=$(bc -l <<< "$ASSUMED_FREQ * $passed_millis / $passed_device_millis")
-    else
-        freq="(not available)"
-    fi
-    #freq=$(($ASSUMED_FREQ * passed_millis / passed_device_millis))
+    freq=$(bc -l <<< "$ASSUMED_FREQ * $passed_device_millis / $passed_millis")
     echo "$current_time ($passed_millis) <=> $device_time ($passed_device_millis) = $new_diff. Freq: $freq"
     if read -t 0.3; then break; fi
 done
