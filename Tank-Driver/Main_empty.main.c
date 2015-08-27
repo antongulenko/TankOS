226a7930c7303fe92f70ede14147f63241672940
Anton Gulenko <anton.gulenko@googlemail.com>
2015-08-27 22:54:37 +0200

    finished driver

--- Tank-Driver/Main_empty.main.c
+++ Tank-Driver/Main_empty.main.c
@@ -3,5 +3,16 @@
 #include <platform/Avr/buffer_stdout.h>
 
 int main() {
-	buffer_stdout_flush_to_eeprom((void*) 2, 256);
+}
+
+void motor_test() {
+	regulateSpeedBackward(leftMotor, 0xffff);
+	regulateSpeedBackward(rightMotor, 0xffff);
+	delay_ms(2000);
+	while (1) {
+		regulateSpeedForward(leftMotor, 0xffff);
+		delay_ms(3500);
+		regulateSpeedBackward(leftMotor, 0xffff);
+		delay_ms(3500);
+	}
 }
