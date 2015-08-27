ca4ca44b83b8898f2959c16a0e0dab356ce3757c
Anton Gulenko <anton.gulenko@googlemail.com>
2015-08-27 22:33:57 +0200

    fixed printing

--- TankOS/twi/services/example.c
+++ TankOS/twi/services/example.c
@@ -3,7 +3,7 @@
 #include "example.h"
 
 int test_read_value_format(int (*print)(const char *fmt, ...), void *results, uint16_t results_length) {
-    if (results_length != sizeof(int32_t)) return 0;
+    if (results_length != sizeof(int16_t)) return 0;
     int16_t *res = (int16_t*) results;
     return print("Test value: %i", (int) *res);
 }
