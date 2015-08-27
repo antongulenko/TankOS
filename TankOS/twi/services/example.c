55bd262d84619105fb34b3ae2b9b0238d12b6f99
Anton Gulenko <anton.gulenko@googlemail.com>
2015-08-27 21:20:21 +0200

    changed example service to 16 bit value

--- TankOS/twi/services/example.c
+++ TankOS/twi/services/example.c
@@ -4,6 +4,6 @@
 
 int test_read_value_format(int (*print)(const char *fmt, ...), void *results, uint16_t results_length) {
     if (results_length != sizeof(int32_t)) return 0;
-    int32_t *res = (int32_t*) results;
-    return print("Test value: %u", *res);
+    int16_t *res = (int16_t*) results;
+    return print("Test value: %i", (int) *res);
 }
--- TankOS/twi/services/example.kernel.c
+++ TankOS/twi/services/example.kernel.c
@@ -3,21 +3,21 @@
 #include <stdio.h>
 #include <twi/rpc/server_handler_functions.h>
 
-static int32_t test_value;
+static volatile int16_t test_value;
 
-static RpcHandlerStatus test_write_value_handler(int32_t *args, uint16_t size) {
-    if (size != sizeof(int32_t)) {
+static RpcHandlerStatus test_write_value_handler(int16_t *args, uint16_t size) {
+    if (size != sizeof(int16_t)) {
         return TWI_RPC_handler_illegal_parameters;
     }
     test_value = *args;
     printf("Written value: %i\n", (int) test_value);
     return TWI_RPC_handler_ok;
 }
-TWI_RPC_SERVER_FUNCTION_VOID(test_write_value_handler, TEST_WRITE_VALUE_OPERATION, int32_t)
+TWI_RPC_SERVER_FUNCTION_VOID(test_write_value_handler, TEST_WRITE_VALUE_OPERATION, int16_t)
 
 static RpcHandlerStatus test_read_value_handler(TWIBuffer *results) {
     printf("Reading value: %i\n", (int) test_value);
-    FILL_RESULT(results, int32_t, test_value)
+    FILL_RESULT(results, int16_t, test_value)
     return TWI_RPC_handler_ok;
 }
 TWI_RPC_SERVER_FUNCTION_NOARGS(test_read_value_handler, TEST_READ_VALUE_OPERATION)
