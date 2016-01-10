4f236ffef78d5dbccadcc47f5d6fbb917aad6d37
Anton Gulenko <anton.gulenko@googlemail.com>
2016-01-10 22:55:18 +0100

    Experimenting

--- Tank-Arm/Main.main.c
+++ Tank-Arm/Main.main.c
@@ -4,40 +4,55 @@
 #include <platform/platform_Avr/port.h>
 
 #include <stdio.h>
+#include <kernel/millisecond_clock.h>
 
-Led ledFront;
-Led ledBack;
-Led ledEnc;
+Led led1;
+Led led2;
+Led led3;
+
+static void initleds() {
+	led1 = newLed(pinC7);
+	led2 = newLed(pinC6);
+	led3 = newLed(pinC5);
+}
 
 static void loopleds() {
 	BOOL on = TRUE;
-	for (int i = 0; i < 3; i++) {
-		setLed(ledFront, on);
-		setLed(ledBack, !on);
-		setLed(ledEnc, on);
+	for (uint8_t i = 0; i < 10; i++) {
+		setLed(led1, on);
+		setLed(led2, !on);
+		setLed(led3, on);
 		on = !on;
-		delay_ms(30);
+		
+		// wait_milliseconds(200);
+		delay_ms(200);
+		
 		// for (int j = 0; j < 2; j++) {
 			// delay_us(10);
 		// }
 	}
 }
 
+int main() {
+	initleds();
+	loopleds();
+	disableLed(led1);
+	disableLed(led2);
+	disableLed(led3);
+}
+
 static void init() {
+	initleds();
 	stepMotorSetMaxSpeed(tank_joint.motor, 3000);
 
 	DDRC |= 0b11111100;
 
-	ledFront = newLed(pinC7);
-	ledBack = newLed(pinC6);
-	ledEnc = newLed(pinC5);
-
 	printf("OK:%i,%i,%i,%i,%i,%i\n", IsValid(tank_socket.back),
 		IsValid(tank_socket.front),
 		IsValid(tank_socket.encoder),
-		IsValid(ledFront),
-		IsValid(ledBack),
-		IsValid(ledEnc));
+		IsValid(led1),
+		IsValid(led2),
+		IsValid(led3));
 
 	loopleds();
 }
@@ -52,20 +67,20 @@ int test_motors_main() {
 	}
 }
 
-int main() {
+int tank_calibrate_main() {
 	init();
 
 	calibrateTankArm(&tank_joint);
 	//disableStepMotor(tank_joint.motor);
 
 	while (1) {
-		setLed(ledFront, hallSensorState(tank_joint.front));
-		setLed(ledBack, hallSensorState(tank_joint.back));
+		setLed(led1, hallSensorState(tank_joint.front));
+		setLed(led2, hallSensorState(tank_joint.back));
 
 		if (tank_joint.calibration == CalibratingSecond || tank_joint.calibration == CalibratedFull) {
-			enableLed(ledEnc);
+			enableLed(led3);
 		} else {
-			disableLed(ledEnc);
+			disableLed(led3);
 		}
 	}
 }
@@ -75,8 +90,8 @@ int test_hall_encoder_main() {
 	while (1) {
 		BOOL back = hallSensorState(tank_socket.back);
 		BOOL front = hallSensorState(tank_socket.front);
-		setLed(ledBack, !back);
-		setLed(ledFront, !front);
+		setLed(led2, !back);
+		setLed(led1, !front);
 
 		encoder_pos_t pos = encoderState(tank_socket.encoder);
 		PORTC &= 0b11000011;
